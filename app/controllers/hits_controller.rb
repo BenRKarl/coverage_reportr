2fc32d57bc818922bb6d5f1e67100edc7d001376
Ben Karl <benrkarl@gmail.com>
2014-05-17 18:45:10 -0400

    complete CRUDS and applicable pathsfor users, coverage reports and hits

--- app/controllers/hits_controller.rb
+++ app/controllers/hits_controller.rb
@@ -1,47 +1,49 @@
 class HitsController < ApplicationController
 
-def index
-   @hits = Hit.all
-end
+# def index
+#    @hits = Hit.all
+# end
 
 def new
-  @user = User.find(params[:user_id].to_i)
-  @coverage_report = CoverageReport.find(params[:coverage_report_id].to_i)
+  @user = User.find(params[:user][:id].to_i)
+  @coverage_report = CoverageReport.find(params[:coverage_report][:id].to_i)
   @hit = Hit.new
 end
 
 def create
   @user = User.find(params[:user][:id].to_i)
   @coverage_report = CoverageReport.find(params[:coverage_report][:id].to_i)
-  hit = Hit.create(hit_params)
-  @coverage_report.hits << hit
-  redirect_to hit_path(hit, user_id: @user.id, coverage_report_id: @coverage_report.id)
+  @hit = Hit.create(hit_params)
+  @coverage_report.hits << @hit
+  redirect_to edit_hit_path(@hit, user_id: @user.id, coverage_report_id: @coverage_report.id)
 end
 
 def show
-  @user = User.find(params[:user][:id].to_i)
-  @coverage_report = CoverageReport.find(params[:coverage_report][:id].to_i)
+  @user = User.find(params[:user_id].to_i)
+  @coverage_report = CoverageReport.find(params[:coverage_report_id].to_i)
+  @hit = Hit.new
 end
 
 def edit
-  @user = User.find(params[:user_id])
-  @coverage_report = CoverageReport.find(params[:coverage_report_id])
+  @user = User.find(params[:user_id].to_i)
+  @coverage_report = CoverageReport.find(params[:coverage_report_id].to_i)
   @hit = Hit.find(params[:id])
 end
 
 def update
-  # user = User.find(params[:user_id])
-  # coverage_report = CoverageReport.find(params[:id])
-  # coverage_report.update(coverage_report_params)
-  # redirect_to user_path(user)
+  @user = User.find(params[:user][:id].to_i)
+  @coverage_report = CoverageReport.find(params[:coverage_report][:id].to_i)
+  @hit = Hit.find(params[:id])
+  @hit.update(hit_params)
+  redirect_to coverage_report_path(@coverage_report, user_id: @user.id, coverage_report_id: @coverage_report.id)
 end
 
 def destroy
-  user = User.find(params[:user][:id].to_i)
-  coverage_report = CoverageReport.find(params[:coverage_report][:id].to_i)
-  hit = Hit.find(params[:id])
-  hit.delete
-  redirect_to coverage_report_path(coverage_report)
+  @user = User.find(params[:user][:id].to_i)
+  @coverage_report = CoverageReport.find(params[:coverage_report][:id].to_i)
+  @hit = Hit.find(params[:id])
+  @hit.delete
+  redirect_to coverage_report_path(@coverage_report, user_id: @user.id, coverage_report_id: @coverage_report.id)
 end
 
 private
