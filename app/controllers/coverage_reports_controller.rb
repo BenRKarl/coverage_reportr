fb7ecfd1040e79356368c5dda70cde222b94434d
Ben Karl <benrkarl@gmail.com>
2014-05-16 22:36:37 -0400

    almost done with ghetto paths for coverage report crud. coverage_report_path(report, :user_id) returning user_id=>{6=>} for some reason...

--- app/controllers/coverage_reports_controller.rb
+++ app/controllers/coverage_reports_controller.rb
@@ -9,11 +9,12 @@ def create
   @user = User.find(params[:user][:id].to_i)
   coverage_report = CoverageReport.create(coverage_report_params)
   @user.coverage_reports << coverage_report
-  redirect_to coverage_report_path(coverage_report)
+  redirect_to coverage_report_path(coverage_report, user_id: @user.id)
 end
 
 def show
-  @user = User.find(params[:user][:id].to_i)
+  binding.pry
+  @user = User.find(params[:user_id].to_i)
   @coverage_report = CoverageReport.find(params[:id])
   @coverage_reports = @user.coverage_reports.all
   @hits = @coverage_report.hits.all
@@ -28,7 +29,7 @@ def update
   @user = User.find(params[:user][:id].to_i)
   coverage_report = CoverageReport.find(params[:id])
   coverage_report.update(coverage_report_params)
-  redirect_to coverage_report_path(coverage_report)
+  redirect_to coverage_report_path(coverage_report, user_id: @user.id)
 end
 
 def destroy
