fb4dca8ee04887863c06e20b81876a92e52963d1
Ben Karl <benrkarl@gmail.com>
2014-05-19 14:05:44 -0400

    fix login errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    User.find(sesstion[:user_id]) if session[:user_id]
+    User.find(session[:user_id]) if session[:user_id]
   end
 
   def authorize
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ def new
 end
 
 def create
-  user = User.find_by(email: params[:email])
+user = User.find_by(email: params[:email])
 if user && user.authenticate(params[:password])
   session[:user_id] = user.id
   redirect_to user_path(user)
