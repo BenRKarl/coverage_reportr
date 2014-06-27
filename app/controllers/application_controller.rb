5cae8fdbdc89cb6490f855a46d3719261795a1dc
Aldric Giacomoni <trevoke@gmail.com>
2014-06-27 16:57:38 -0400

    A lot of reindenting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
-  require 'csv'
 
   def current_user
     User.find(session[:user_id]) if session[:user_id]
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :email, presence: true, uniqueness: true, email: true
   validates :password, :length => {
-    :within => 8..25,
-    :too_short => "Your password is too short. It must be between 8 and 25 characters.",
-    :too_long => "Your password is too long. It must be between 8 and 25 characters."
-    }
+                                   :within => 8..25,
+                                   :too_short => "Your password is too short. It must be between 8 and 25 characters.",
+                                   :too_long => "Your password is too long. It must be between 8 and 25 characters."
+                                  }
 end
